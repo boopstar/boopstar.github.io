494a5426cfcf7790b9611c33f23c75be46bd1904
boopstar <67384389+boopstar@users.noreply.github.com>
2022-03-24 10:05:09 +0530

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,33 @@
+class A
+{
+public:
+int num1;
+};
+
+class B: virtual public A
+{
+public:
+int num2;
+};
+
+class C: virtual public A
+{
+public:
+int num3;
+};
+
+class D: public B, public C
+{
+public:
+void sum()
+cout<<num1+num2+num3;
+};
+
+int main()
+{
+  D obj;
+  obj.num1=10;
+  obj.num2=20;
+  obj.num3=30;
+  obj.sum();
+}
